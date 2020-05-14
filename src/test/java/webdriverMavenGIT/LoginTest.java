6e77adf01e18cdb121247b138cb6ded9f77ec990
Siddharth Raipure <siddharth.raipure@gmail.com>
2020-05-14 18:27:05 +0530

    Initial commit

---
+++ src/test/java/webdriverMavenGIT/LoginTest.java
@@ -0,0 +1,45 @@
+package webdriverMavenGIT;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+
+
+	@BeforeSuite
+	public void setUp() {
+
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+
+	}
+
+	@Test
+	public void doLoginIn() {
+
+		driver.get("http://www.way2automation.com/angularjs-protractor/registeration/#/login");
+		driver.findElement(By.id("username")).sendKeys("angular");
+		driver.findElement(By.id("password")).sendKeys("password");
+		
+
+	}
+
+
+	@AfterSuite
+	public void tearDown() {
+
+		driver.quit();
+	}
+
+}
